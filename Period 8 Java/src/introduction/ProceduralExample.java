b08dadee1369a985bf48cbccdfb95f92264267cc
Student8 <Student8@10.8.33.106>
2016-09-14 13:56:27 -0400

    testing a paradox

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -2,7 +2,7 @@ package introduction;
 
 public class ProceduralExample {
   public static void main(String[] args){
-    System.out.println("Hello World");
+    System.out.println("Hello World is a bad line");
     System.out.println("Hello World");
   }
 }
